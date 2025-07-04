b8deeefce1de889f0e7e735ce7ceaf0055d959a0
zkoymen <zeynepkoymen9@gmail.com>
2025-07-04 14:38:24 +0300

    mappers created

---
+++ src/main/java/guru/springframework/spring6restmvc/mappers/CustomerMapper.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring6restmvc.mappers;
+
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+
+    Customer customerDtoToCustomer(CustomerDTO dto);
+
+    CustomerDTO customerToCustomerDto(Customer customer);
+}
+
