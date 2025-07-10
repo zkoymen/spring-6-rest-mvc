69dbfa8db65768977eee432f8348aa48f331d93a
zkoymen <zeynepkoymen9@gmail.com>
2025-07-10 17:07:34 +0300

    added database constraint validation and entity as well too catch early

--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -2,6 +2,9 @@ package guru.springframework.spring6restmvc.entities;
 
 import guru.springframework.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -17,6 +20,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Beer {
+    // About db entities
 
     @Id
     @GeneratedValue(generator = "UUID")
@@ -27,10 +31,22 @@ public class Beer {
 
     @Version
     private Integer version;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 50)                 // validating even before trying to save to db
+    @Column(length = 50)
     private String beerName;
+
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotNull
+    @NotBlank
     private String upc;
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,13 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,12 +18,46 @@ class BeerRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
 
+
+
+    @Test
+    void testSaveBeerNameTooLong() {
+
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            // exceed 50 char constraint
+            Beer testBeer = beerRepository.save(Beer.builder()
+                    .beerName("Zeynep BeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDABeerASDASDASDASDAv")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("12421454656")
+                    .price(new BigDecimal(150.50))
+                    .build());
+
+            // running to quickly it skips
+            // flush --> it immediately writes to the db
+            beerRepository.flush();
+        });
+
+    }
+
+
+
     @Test
     void testSaveBeer() {
 
+
+        // add other properties to not fail the test
         Beer testBeer = beerRepository.save(Beer.builder()
                         .beerName("Zeynep Beer")
+                        .beerStyle(BeerStyle.PALE_ALE)
+                        .upc("12421454656")
+                        .price(new BigDecimal(150.50))
                 .build());
+
+
+        // running to quickly it skips
+        // flush --> it immediately writes to the db
+        beerRepository.flush();
 
         // Make sure it has an id
         assertThat(testBeer).isNotNull();
